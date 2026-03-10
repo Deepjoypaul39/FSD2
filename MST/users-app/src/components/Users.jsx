5aa9eba0160daf7f75de733a6e17d7c08011722e
Deepjoypaul39 <23bai70442@cuchd.in>
2026-03-10 11:52:46 +0530

    MST-Q-2

---
+++ MST/users-app/src/components/Users.jsx
@@ -0,0 +1,18 @@
+function Users() {
+  return (
+    <div className="card">
+
+      <h1>Lab Practical MST</h1>
+
+      <p><b>University:</b> Chandigarh University, Punjab</p>
+      <p><b>Name:</b> Deepjoy Paul</p>
+      <p><b>UID:</b> 23BAI70442</p>
+      <p><b>Subject:</b> Full Stack - 2</p>
+      <p><b>Teacher Name:</b> Pritam Das</p>
+      <p><b>Date:</b> 10-03-2026</p>
+
+    </div>
+  );
+}
+
+export default Users;
